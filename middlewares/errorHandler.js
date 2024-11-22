ee88a31ab240b564a2bbd07b451b8b45bfb58f5a
Azaan-Ayaz <azaanayaz8@gmail.com>
2024-11-22 12:50:08 +0500

    Console Error

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -4,7 +4,7 @@ export default (err, req, res, next) => {
     console.error('Error:', err);
   
     res.status(500).json({
-      error: 'An unexpected error occurred. Please try again later.',
+      error: `An unexpected error occurred. Please try again later. ${err}`,
     });
   };
   
